3c21edc89d5b92b10b88c1947982ae6b8540da40
Nicholas Crossman <nicholascrossman@gmail.com>
2021-10-07 13:55:32 -0700

    Added transfer() method to dollars_bank_atm to allow money to be moved from one customer to another.

--- src/dollars_bank_atm.js
+++ src/dollars_bank_atm.js
@@ -10,4 +10,29 @@ class dollars_bank_atm {
         let account = new customer_account(email, firstName, lastName, password, initialBalance);
         this.accounts.push(account);
     }
+
+    transfer = (account1, account2, amount) => {
+        // make sure the amount is positive
+        if(amount < 0) {
+            return false;
+        }
+        // transfer money from account1 into account2
+        // first make sure account1 has the monmey required
+        const moneyAvailable = account1.balance > amount;
+        if(moneyAvailable) {
+            // get the money from account 1
+            let withdrawlSuccess = account1.transaction(amount * -1, "Transfer to " + account2.email);
+            if(withdrawlSuccess) {
+                // trandfer the money to account2
+                let transferSuccess = account2.transaction(amount, "Transfer from " + account1.email);
+                if(transferSuccess) {
+                    return true;
+                }
+            }
+        }
+        // account1 lacks the funds to transfer
+        return false;
+    }
 }
+
+export default dollars_bank_atm;
